9395e2286964e9c9992779d10eeec3f9bee8d46d
EC2 Default User <ec2-user@ip-172-31-14-172.eu-west-1.compute.internal>
2018-09-23 19:16:21 +0000

    Section 2 lecture 26 - query youtube, sketch of video list

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar.js'
+import VideoList from './components/video_list.js'
 
 const API_KEY = 'AIzaSyA2jvSX--7iPnmaBhAZHeuIBp6ioVAqpR8'
 
 // Create a new component
 // This component should produce some HTML
 
-const App = () => {
-    return (
+class App extends Component {
+    constructor (props) {
+        super(props);
+        
+        this.state = { videos: []}
+        
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos })
+        });
+    }
+    
+    render() {
+        return (
             <div>
                 <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
+    }
 }
 
 // Get components generated html into the DOM
