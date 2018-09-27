d24289be5fce3cef4a374acb9d29a602fcc35268
EC2 Default User <ec2-user@ip-172-31-14-172.eu-west-1.compute.internal>
2018-09-27 20:48:03 +0000

    Reset to Steven's example but with my amazon config

--- src/index.js
+++ src/index.js
@@ -1,53 +1,15 @@
-import _ from 'lodash'
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom'
-import YTSearch from 'youtube-api-search'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-import SearchBar from './components/search_bar.js'
-import VideoList from './components/video_list.js'
-import VideoDetail from './components/video_detail.js'
+import App from './components/app';
+import reducers from './reducers';
 
-const API_KEY = 'AIzaSyA2jvSX--7iPnmaBhAZHeuIBp6ioVAqpR8'
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-// Create a new component
-// This component should produce some HTML
-
-class App extends Component {
-    constructor (props) {
-        super(props);
-        
-        this.state = {
-                        videos: [],
-                        selectedVideo: null
-                    }
-                    
-        this.videoSearch('surfboards')
-    }
-    
-    videoSearch(term) {
-        
-        YTSearch({key: API_KEY, term: term}, (videos) => {
-            this.setState({
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-        });
-    }
-    
-    render() {
-        const videoSearch = _.debounce((term) => {this.videoSearch(term)},300);
-        
-        return (
-            <div>
-                <SearchBar onSearchTermChange={videoSearch}  />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList 
-                    onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-                    videos={this.state.videos} />
-            </div>
-        );
-    }
-}
-
-// Get components generated html into the DOM
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('.container'));
