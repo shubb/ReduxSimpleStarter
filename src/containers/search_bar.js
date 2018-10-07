51aa79c1392501d0184365396bc0fc27dbde707c
EC2 Default User <ec2-user@ip-172-31-14-172.eu-west-1.compute.internal>
2018-10-07 05:46:57 +0000

    Section 5 lecture 59

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index.js'
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 
     constructor(props) {
         super(props);
 
-        this.state = { term: '' }
-        this.onInputChange = this.onInputChange.bind(this)
+        this.state = { term: '' };
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
-        console.log(event.target.value)
-        this.setState({ term: event.target.value })
+        console.log(event.target.value);
+        this.setState({ term: event.target.value });
     }
 
     onFormSubmit(event) {
         event.preventDefault();
+
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' });
     }
 
     render() {
@@ -31,6 +38,12 @@ export default class SearchBar extends Component {
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
              </form>
-        )
+        );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
